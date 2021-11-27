e34270b563138170765349f2c0b2dc4bd1e99c83
ROMIS21 <90635567+ROMIS21@users.noreply.github.com>
2021-11-27 23:11:22 +0530

    Update Main.java

--- BAnk/src/com/bank/Main.java
+++ BAnk/src/com/bank/Main.java
@@ -1,37 +1,12 @@
-package com.bank;
-
-import java.util.Scanner;
+package com.banking;
 
 public class Main {
-	
-	
 
-	    public static void main(String[] args) {
+	 public static void main(String[] args) {
 
-	        Account abhaysAccount = new Account("Number",0.0,"Name","Email","PhoneNumber");
-	        Scanner sc = new Scanner(System.in);
-	        
-	        System.out.println("Please Enter your Account Number: ");
-	        String Number=sc.next();   
-	        
-	        System.out.println("Please Enter your Name: ");
-	        String Name=sc.next();
-	        
-	        System.out.println("Please Enter your Email you want to register with Account: ");
-	        String Email=sc.next();
-	        
-	        System.out.println("Please Enter your Phone Number you want to register with Account:: ");
-	        String PhoneNumber=sc.next();
-	        
-	        System.out.println("Please enter the amount you want to deposite");
-	        double Balance= sc.nextDouble();
-	        
+	        Account account = new Account("Number",0.0d,"Name","Email","PhoneNumber", 0.0d, 0.0d);
 
-	        abhaysAccount.DepositMoney(0);
-	        abhaysAccount.DepositMoney(0);
-
-	        abhaysAccount.WithDrawMoney(0);
 	    
-	    }
-	}
-
+	        
+}
+}
